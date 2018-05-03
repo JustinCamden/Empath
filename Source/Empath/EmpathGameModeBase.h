fdb61a7db5c14a3f7bb445b25599fffd3ac40fdc
Unknown <camden@usc.edu>
2018-05-03 11:03:16 -0700

    Initializing project

---
+++ Source/Empath/EmpathGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EmpathGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMPATH_API AEmpathGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
