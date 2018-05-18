6798f47dd60945f120db7ce416bd7ed5e4e03aa4
Unknown <camden@usc.edu>
2018-05-18 16:14:23 -0700

    Added custom aim location interface

---
+++ Source/Empath/Private/AImLocationInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright 2018 Team Empath All Rights Reserved
+
+#include "AImLocationInterface.h"
+
+
+// Add default functionality here for any IAImLocationInterface functions that are not pure virtual.
---
+++ Source/Empath/Public/EmpathAimLocationInterface.h
@@ -0,0 +1,27 @@
+// Copyright 2018 Team Empath All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "EmpathAimLocationInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UEmpathAimLocationInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class EMPATH_API IEmpathAimLocationInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	
+	
+};
