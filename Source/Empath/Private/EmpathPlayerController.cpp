14accf183ee422e75d85aad2b9441d8afc58e2d3
Unknown <camden@usc.edu>
2018-05-18 17:15:36 -0700

    Made AI realize when one of them has been hit

---
+++ Source/Empath/Private/EmpathPlayerController.cpp
@@ -0,0 +1,7 @@
+// Copyright 2018 Team Empath All Rights Reserved
+
+#include "EmpathPlayerController.h"
+
+
+
+
---
+++ Source/Empath/Public/EmpathPlayerController.h
@@ -0,0 +1,20 @@
+// Copyright 2018 Team Empath All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "EmpathPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMPATH_API AEmpathPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
